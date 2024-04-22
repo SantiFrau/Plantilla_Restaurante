7bc7f78ec5b7269df704526c5c55a65f4737f29a
Santi <santiagofrau3@gmail.com>
2024-04-22 17:37:02 -0300

    config

--- src/data/menu_info.js
+++ src/data/menu_info.js
@@ -1,19 +1,19 @@
 export const menu = [
     {
         name:"Hamburguesa Clasica",
-        img:"/menu/1/m.webp",
+        img:"/Plantilla_Restaurante/menu/1/m.webp",
         precio:2.9,
         descripcion:""
     },
     {
         name:"Hamburguesa Triple",
-        img:"/menu/2/m.webp",
+        img:"/Plantilla_Restaurante/menu/2/m.webp",
         precio:4.9,
         descripcion:""
     },
     {
         name:"Clasica de pollo",
-        img:"/menu/3/m.webp",
+        img:"/Plantilla_Restaurante/menu/3/m.webp",
         precio:2.6,
         descripcion:""
     }
